7668c2ba660a31ac803793c09ef2030b203bd088
EC2 Default User <ec2-user@ip-172-31-23-12.ap-northeast-1.compute.internal>
2023-06-09 07:11:26 +0000

    commit5

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+  get "search" => "searches#search"
+  resources :books, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources:book_comments,only:[:create,:destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resources :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
